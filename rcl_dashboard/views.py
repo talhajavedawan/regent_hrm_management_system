fe265111d82c535ffca0e21068ae3340208205a9
master <talhajavedawan7@gmail.com>
2026-01-15 09:48:34 +0000

    Added Employee models

--- rcl_dashboard/views.py
+++ rcl_dashboard/views.py
@@ -25,3 +25,77 @@ def hr_login(request):
             login(request, user)
             return redirect('hr_dashboard')
     return render(request, 'hr/login.html')
+# ---------------- EMPLOYEE LOGIN ----------------
+def employee_login(request):
+    if request.method == 'POST':
+        user = authenticate(
+            username=request.POST['username'],
+            password=request.POST['password']
+        )
+        if user and not user.is_staff:
+            login(request, user)
+            return redirect('employee_dashboard')
+    return render(request, 'employee/login.html')
+
+
+# ---------------- ADD EMPLOYEE (HR) ----------------
+
+
+@login_required
+def add_employee(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        department = request.POST['department']
+        nationality = request.POST['nationality']
+        share_code = request.POST.get('share_code')
+
+        # 🇬🇧 UK employee → skip visa check
+        if nationality == 'United Kingdom':
+            Employee.objects.create(
+                name=name,
+                department=department,
+                nationality=nationality,
+                visa_status='VALID',
+                share_code="Not Required"
+            )
+            print("hello")
+            return redirect('/hr/employees/')
+
+        # 🌍 Non-UK → Share code mandatory
+        if not share_code:
+            return render(request, 'hr/error.html', {
+                'msg': 'Share code is required for non-UK employees.'
+            })
+
+        # Check visa using share code
+        try:
+            visa = VisaExpiry.objects.get(share_code=share_code)
+        except VisaExpiry.DoesNotExist:
+            return render(request, 'hr/error.html', {
+                'msg': 'Invalid share code. Visa record not found.'
+            })
+
+        # Check expiry
+
+        if visa.expiry_date < timezone.now().date():
+            return render(request, 'hr/error.html', {
+                'msg': 'Visa is expired. Employee cannot be added.'
+            })
+        user = User.objects.create_user(
+            username=name,
+            password='emp123'
+        )
+
+        # Valid visa → add employee
+        Employee.objects.create(
+             user=user,
+            name=name,
+            department=department,
+            nationality=nationality,
+            share_code=share_code,
+            visa_status='VALID'
+        )
+
+        return redirect('/hr/employees/')
+
+    return render(request, 'hr/add_employee.html')
