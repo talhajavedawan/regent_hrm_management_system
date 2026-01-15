66c81edb1086067d922045013f0fe14bd3665de3
master <talhajavedawan7@gmail.com>
2026-01-15 10:26:23 +0000

    Added model registration in admin

--- rcl_dashboard/admin.py
+++ rcl_dashboard/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from  .models import Employee,VisaExpiry,SalarySlip,Leave
+
+admin.site.register(Employee)
+admin.site.register(VisaExpiry)
+admin.site.register(SalarySlip)
+admin.site.register(Leave)
