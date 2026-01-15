5ade092d3f71c7302a18e8641043be1d89f3184d
Arun <arunarun1995010@gmail.com>
2026-01-15 09:43:29 +0000

    changes in file -added login logic at views file

--- rcl_dashboard/views.py
+++ rcl_dashboard/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,get_object_or_404
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from datetime import date
+from .models import *
+from django.contrib.auth import logout
+from django.shortcuts import redirect, render
+from .models import Announcement, Notification, Leave
+from django.utils import timezone
 
-# Create your views here.
-# Create your views here.
+from .models import Employee, VisaExpiry
+from django.shortcuts import render, redirect
+
+def home_page(request):
+    return render(request, 'home.html')
+# ---------------- HR LOGIN ----------------
+def hr_login(request):
+    if request.method == 'POST':
+        user = authenticate(
+            username=request.POST['username'],
+            password=request.POST['password']
+        )
+        if user and user.is_staff:
+            login(request, user)
+            return redirect('hr_dashboard')
+    return render(request, 'hr/login.html')
